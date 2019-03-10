2ac54e9c00f4c78a87e2078e39c37c0a8e0b5835
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-03-10 16:00:42 +0200

    add

--- src/App.js
+++ src/App.js
@@ -4,28 +4,18 @@ import Header from './Header';
 import './App.css';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      input: '',
-    }
-  }
 
-  onInputChange = (event) => {
-    console.log(event);
-  }
-
-  componentDidMount() {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=%D7%A8%D7%97%D7%95%D7%91%D7%95%D7%AA&type=accurate&APPID=de6b1890ad2f21aebdbfb8e1bd667add')
-    .then(response=> response.json())
-    .then(zxc => console.log(zxc));
-  }
+  // componentDidMount() {
+  //   fetch('https://api.openweathermap.org/data/2.5/weather?q=%D7%A8%D7%97%D7%95%D7%91%D7%95%D7%AA&type=accurate&APPID=de6b1890ad2f21aebdbfb8e1bd667add')
+  //   .then(response=> response.json())
+  //   .then(zxc => console.log(zxc));
+  // }
 
   render() {
     return (
       <div className="allApp" >
         <Header />
-        <ApplicationCenter onInputChange={this.onInputChange} />
+        <ApplicationCenter />
       </div>
     );
   }
--- src/ApplicationCenter.js
+++ src/ApplicationCenter.js
@@ -9,8 +9,14 @@ class ApplicationCenter extends React.Component {
     }
   }
 
-  onInputChange = (event) => {
-    console.log(event);
+  onInputChange = () => {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=%D7%A8%D7%97%D7%95%D7%91%D7%95%D7%AA&type=accurate&APPID=de6b1890ad2f21aebdbfb8e1bd667add')
+    .then(response=> response.json())
+    .then(zxc => console.log(zxc));
+  }
+
+  onButtonSubmit = () => {
+    console.log("click");
   }
 
   render() {
@@ -18,7 +24,7 @@ class ApplicationCenter extends React.Component {
       <div className="ApplicationCenter" >
         <h1>רשום עיר או מדינה</h1>
         <input type='search' placeholder='שם עיר' onChange={this.onInputChange} />
-        <input type='button' className="button1" value="קבל את התחזית"/>
+        <input type='button' className="button1" value="קבל את התחזית" onClick={this.onButtonSubmit}/>
       </div>
     );
   }
