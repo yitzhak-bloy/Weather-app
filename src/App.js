96286058c80714ec3d7d26adb1e958aea007a2aa
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-03-13 15:00:08 +0200

    add

--- src/App.js
+++ src/App.js
@@ -4,18 +4,39 @@ import Header from './Header';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      city: '',
+    }
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onButtonSubmit = this.onButtonSubmit.bind(this);
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value})
+  }
 
   // componentDidMount() {
-  //   fetch('https://api.openweathermap.org/data/2.5/weather?q=%D7%A8%D7%97%D7%95%D7%91%D7%95%D7%AA&type=accurate&APPID=de6b1890ad2f21aebdbfb8e1bd667add')
-  //   .then(response=> response.json())
-  //   .then(zxc => console.log(zxc));
+  //   this.setState({city: this.state.input})
   // }
+
+  onButtonSubmit = () => {
+    this.setState({city: this.state.input})
+    console.log(this.state.city);
+   if (this.state.city !== '') {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&type=accurate&APPID=de6b1890ad2f21aebdbfb8e1bd667add`)
+      .then(response=> response.json())
+      .then(zxc => console.log(zxc));
+    }
+  }
 
   render() {
     return (
       <div className="allApp" >
         <Header />
-        <ApplicationCenter />
+        <ApplicationCenter onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
       </div>
     );
   }
--- src/ApplicationCenter.js
+++ src/ApplicationCenter.js
@@ -1,51 +1,14 @@
 import React from 'react';
 import './ApplicationCenter.css';
 
-class ApplicationCenter extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      input: '',
-      city: '',
-    }
-  }
-
-  componentDidMount() {
-
-  }
-
-  onInputChange = (event) => {
-    this.setState({input: event.target.value})
-    this.setState({city: this.state.input})
-  }
-
-  onButtonSubmit = () => {
-    console.log(this.state.city);
-   if (this.state.city !== '') {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&type=accurate&APPID=de6b1890ad2f21aebdbfb8e1bd667add`)
-      .then(response=> response.json())
-      .then(zxc => console.log(zxc));
-    }
-  }
-
-  // componentDidMount() {
-  //   if (this.state.city === 'רחובות') {
-  //     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&type=accurate&APPID=de6b1890ad2f21aebdbfb8e1bd667add`)
-  //     .then(response=> response.json())
-  //     .then(zxc => console.log(zxc));
-  //   }
-  // }
-
-  render() {
+  const ApplicationCenter = ({ onInputChange, onButtonSubmit}) => {
     return (
       <div className="ApplicationCenter" >
         <h1>רשום עיר או מדינה</h1>
-        <input type='search' placeholder='שם עיר' onChange={this.onInputChange} />
-        <input type='button' className="button1" value="קבל את התחזית" onClick={this.onButtonSubmit}/>
+        <input type='search' placeholder='שם עיר' onChange={onInputChange} />
+        <input type='button' className="button1" value="קבל את התחזית" onClick={onButtonSubmit}/>
       </div>
     );
   }
-} 
-  
+
   export default ApplicationCenter;
-  
